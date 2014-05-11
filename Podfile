1e9c6ca41a81c3dda5e57408fe475dd9a4685266
DJBen <lsh32768@gmail.com>
2014-05-11 05:07:20 -0400

    Add screenshot

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, "7.0"
+
+target :m2048Tests, :exclusive => true do
+  pod 'Kiwi/XCTest'
+end
---
+++ Pods/Pods-m2048Tests-Kiwi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_m2048Tests_Kiwi : NSObject
+@end
+@implementation PodsDummy_Pods_m2048Tests_Kiwi
+@end
