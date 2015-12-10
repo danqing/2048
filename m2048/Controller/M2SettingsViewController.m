5aea763dfd19c51c1403a856d95bf5f16b495280
Christopher Stott <christopherstott@gmail.com>
2015-12-09 17:16:15 -0800

    Hello Portland!

--- m2048/Controller/M2SettingsViewController.m
+++ m2048/Controller/M2SettingsViewController.m
@@ -59,7 +59,7 @@
 - (void)viewDidLoad
 {
   NSLog(@"M2SettingsViewController - viewDidLoad");
-  NSLog(@"trying something risky");
+  NSLog(@"trying something risky!");
   strcpy(0, "this is a bad bug");
   [super viewDidLoad];
   self.navigationController.navigationBar.tintColor = [GSTATE scoreBoardColor];
